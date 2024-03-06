2d75b2b4fcbc8d61685ec3495e1bf73c61ae1abf
samuel <leumasmontreal@hotmail.com>
2024-03-05 19:00:40 -0500

    .

--- parcInfo.sql
+++ parcInfo.sql
@@ -1,49 +1,45 @@
 begin transaction;
 
-CREATE TABLE Segment(
-    indIP int PRIMARY KEY ,
-    nomSegment varchar(255) NOT NULL,
-    etage int
+CREATE TABLE Segment( 
+		 indIP varchar(15) PRIMARY KEY,
+         nomSegment varchar(255) NOT NULL,
+         etage int
+		);
+
+CREATE TABLE Salle (
+    nSalle INT PRIMARY KEY,
+    nomSalle VARCHAR(255) NOT NULL,
+    nbPoste INT,
+    intIP INT
 );
 
-CREATE TABLE Salle(
-    nSalle int PRIMARY KEY,
-    nomSalle varchar(255) NOT NULL,
-    nbPoste int,
-    intIP int,
+CREATE TABLE Poste (
+    nPoste INT PRIMARY KEY,
+    nomPoste VARCHAR(255) NOT NULL,
+    ad VARCHAR(255)
 );
 
-CREATE TABLE Poste(
-    nPoste int PRIMARY KEY,
-    nomPoste varchar(255) NOT NULL,
-    ad varchar(255),
+CREATE TABLE Logiciel (
+    nLog INT PRIMARY KEY,
+    nomLog VARCHAR(255),
+    dateAch DATE,
+    version VARCHAR(255),
+    typeLog VARCHAR(255),
+    prix FLOAT CHECK (prix >= 0)
 );
 
-CREATE TABLE Logiciel(
-    nLog int PRIMARY KEY,
-    nomLog varchar(255),
-    dateAch Date,
-    version varchar(255),
-    typeLog varchar(255),
-    prix float CHECK (prix >= 0)
+
+CREATE TABLE Installer( 
+	numsIns SERIAL PRIMARY KEY,
+    dateIns timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    delai int,
+	nomPoste VARCHAR(255) CHECK (nomPoste >= '0' AND nomPoste <= '255'
+	)
 );
+	
 
-CREATE TABLE Installer(
-    numsIns SERIAL PRIMARY KEY,
-    dateIns timestamp NOT NULL DEFAULT GETDATE(),
-    delai int
-    CONSTRAINT chk_nomPoste_range CHECK (
-    CASE
-        WHEN nomPoste >= '0' AND nomPoste <= '255' THEN 1
-        ELSE 0
-    END = 1
-    )
-
-);
-CREATE TABLE Types(
-    typeLp varchar(255) PRIMARY KEY,
-    nomType varchar(255)
-)
-
+CREATE TABLE Types( typeLp varchar(255) PRIMARY KEY,
+                                                nomType varchar(255));
 
 commit;
+
--- references.sql
+++ references.sql
@@ -16,5 +16,39 @@
 */
 BEGIN TRANSACTION;
 
+ALTER TABLE Poste
+ADD CONSTRAINT FK_Poste_Segment
+FOREIGN KEY (#indIP)
+REFERENCES Segment(#indIP);
+
+ALTER TABLE Poste
+ADD CONSTRAINT FK_Poste_Types
+FOREIGN KEY (#typePoste)
+REFERENCES Types(#typePoste);
+
+ALTER TABLE Poste
+ADD CONSTRAINT FK_Poste_Salle
+FOREIGN KEY (#nSalle)
+REFERENCES Salle(#nSalle);
+
+ALTER TABLE Installer
+ADD CONSTRAINT FK_Installer_Poste
+FOREIGN KEY (#nPoste)
+REFERENCES Poste(#nPoste);
+
+ALTER TABLE Installer
+ADD CONSTRAINT FK_Installer_Logiciel
+FOREIGN KEY (#nLog)
+REFERENCES Logiciel(#nLog);
+
+ALTER TABLE Logiciel
+ADD CONSTRAINT FK_Logiciel_Types
+FOREIGN KEY (#typelog)
+REFERENCES Types(#typelog);
+
+ALTER TABLE Salle
+ADD CONSTRAINT FK_Salle_Segment
+FOREIGN KEY (#indIP)
+REFERENCES Segment(#indIP);
 
 COMMIT;
